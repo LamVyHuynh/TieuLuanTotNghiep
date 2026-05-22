9bb8972a975974a1eff938a8d5587e28444e905a
LamVyHuynh <huynhlamvy145@gmail.com>
2026-05-22 16:20:33 +0700

    sửa lại 1 hạt sạn nhỏ

--- web-nong-san/src/pages/admin/Categories.jsx
+++ web-nong-san/src/pages/admin/Categories.jsx
@@ -38,7 +38,7 @@ function CategoriesPage() {
     name: "",
     description: "",
     image_url: "",
-    status: "active",
+    status: 1, // Mặc định là hiển thị (active)
   };
   const [formData, setFormData] = useState(initialFormState);
 
